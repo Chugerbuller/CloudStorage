199174705991351782d925551e6afa958af4f241
Andrey Chugainov <sprunk223@gmail.com>
2025-02-17 19:27:45 +0500

    -add ui

---
+++ CloudStore.UI/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using CloudStore.UI.ViewModels;
+using CloudStore.UI.Views;
+
+namespace CloudStore.UI
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new LoginWindow
+                {
+                    DataContext = new LoginWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ CloudStore.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace CloudStore.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
